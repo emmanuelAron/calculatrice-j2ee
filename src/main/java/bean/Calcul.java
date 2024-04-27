5613a764b1dc08b0cd6919de0daec49026e20942
Aron <emmanuel.aron@gmail.com>
2024-04-27 17:38:23 +0200

    Modifier image rest service

--- src/main/java/bean/Calcul.java
+++ src/main/java/bean/Calcul.java
@@ -2,6 +2,14 @@ package bean;
 
 import jakarta.xml.bind.annotation.XmlRootElement;
 
+/***
+ * Utilisation d'un record (apparu avec java 16, au lieu d'une classe java classique).
+ * Les records sont immutables.
+ * L'annotation @XmlRootElement permet de l'utiliser avec notre web service pour effectuer une sérialisation XML (Objet vers XML).
+ * @author emman
+ *
+ */
+
 @XmlRootElement
 public record Calcul(int operande1, String signe, int operande2) {
     
