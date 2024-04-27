95dbb0bbd1292eb2f0a655e04e34bfae8fb402e0
Aron <emmanuel.aron@gmail.com>
2024-04-27 10:30:54 +0200

    Formattage CalculService pour retourner le résultat en XML

--- src/main/java/service/CalculService.java
+++ src/main/java/service/CalculService.java
@@ -17,24 +17,12 @@ public class CalculService {
 	@Produces(MediaType.APPLICATION_XML)
 	public Response getResultXML(@QueryParam("operande1") int operande1 , @QueryParam("operande2") int operande2 , @QueryParam("signe") String signe) {
 		
-		//Calcul c = new Calcul(3, "+", 4);
-		
 		Calcul c = new Calcul(operande1, signe, operande2);
 		int result = c.result();
 		
-		//return Response.ok().entity(String.valueOf(result)).build();
-		return Response.ok().entity(c).build();
+		return Response.ok().entity("<resultat>" + result + "</resultat>").build();
+
 	}
 	
-	
-	/*
-	 * @GET
-	 * 
-	 * @Path("/test")
-	 * 
-	 * @Produces(MediaType.TEXT_PLAIN) public String testEndpoint() {
-	 * System.out.println("test endpoint"); return
-	 * "Test endpoint accessed successfully!"; }
-	 */
 
 }
